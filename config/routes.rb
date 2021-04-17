d26e6d9a73129ef285e3c4cf3d5a89161c14d8a2
shuto3726 <sss3726mmm@gmail.com>
2021-04-17 07:04:07 +0000

    start 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
- root to: 'homes#top'
- devise_for :users
- resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-  resource :favorites, only: [:create, :destroy]
-  resources :post_comments, only: [:create, :destroy]
- end
- 
+  get 'users/show'
+  root to: 'homes#top'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resource :users, only: [:show]
 end
