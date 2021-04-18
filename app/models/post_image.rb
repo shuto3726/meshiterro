38a8f18ceed2ffb6db50583762c8edd556dc286b
shuto3726 <sss3726mmm@gmail.com>
2021-04-18 01:02:09 +0000

    start 3-6

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  varidates :shop_name, presence: true
+  varidates :image, presence: true
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
